6a6974ba9a3ac1b6035b6468109c22401cbcad0f
Divyabharath-27 <119445402+Divyabharath-27@users.noreply.github.com>
2023-03-05 21:11:21 -0500

    Add files via upload

---
+++ Delete.java
@@ -0,0 +1,40 @@
+package practice4;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class Delete extends salesforceBaseClass  {
+	@Test
+
+	public void Delete() throws InterruptedException {
+	
+		WebElement all = driver.findElement(By.xpath("//div[@class='slds-accordion__summary']"));
+		 all.click();
+		 WebElement Dash = driver.findElement(By.xpath("//p[text()='Dashboards']/ancestor::li"));
+		 Dash.click();
+		 
+		   WebElement name=driver.findElement(By.xpath("//input[@class='search-text-field slds-input input uiInput uiInputText uiInput--default uiInput--input']"));
+		   name.sendKeys("Divyabharath",Keys.ENTER);
+
+     	    Thread.sleep(3000);
+	        WebElement brands=driver.findElement(By.xpath("(//button[@class='slds-button slds-button_icon-border slds-button_icon-x-small'])[1]//following::lightning-primitive-icon"));
+			Actions move=new Actions(driver);
+			move.moveToElement(brands).perform();
+			brands.click();
+
+			driver.findElement(By.xpath("//span[text()='Delete']")).click();
+		    driver.findElement(By.xpath("//span[text()='Delete']/parent::button")).click();
+			System.out.println();
+		   
+		   
+		   
+
+	}
+
+}
+	
+
